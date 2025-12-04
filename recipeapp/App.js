26c42f3e4c96f921117d23c25fda6c6798da2262
rtpost31006 <rtpost31006@gmail.com>
2025-12-04 11:55:28 -0500

    new changes

--- recipeapp/App.js
+++ recipeapp/App.js
@@ -1,8 +1,16 @@
-import Foods from './Foods';
+import Foods from './foods';
+import Recipe from './Recipe';
+import { StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
     return(
-        
+    <NavigationContainer>
+    <Stack.Navigator initialRouteName='Foods'>
+    <Stack.Screen name='Foods' component={Foods} />
+    <Stack.Screen name='Recipe' component={Recipe} />  
+    </Stack.Navigator>    
+    </NavigationContainer>  
     )
 }
 
--- recipeapp/Foods.js
+++ recipeapp/Foods.js
@@ -1,5 +1,5 @@
-
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
 
 
 const [foods, setfoods] = useState 
