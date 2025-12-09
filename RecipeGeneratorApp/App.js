52f4e88bbf9838de921b9bc8fb68bf419f056bd2
Nicole Wood <nicolewood@Nicoles-MacBook-Air.local>
2025-12-09 18:50:22 -0500

    added to foods

--- RecipeGeneratorApp/App.js
+++ RecipeGeneratorApp/App.js
@@ -1,16 +1,16 @@
-import Foods from './foods';
+import Foods from './Foods';
 import Recipe from './Recipe';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 
 export default function App() {
     return(
-    <NavigationContainer>
-    <Stack.Navigator initialRouteName='Foods'>
-    <Stack.Screen name='Foods' component={Foods} />
-    <Stack.Screen name='Recipe' component={Recipe} />  
-    </Stack.Navigator>    
-    </NavigationContainer>  
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName='Foods'>
+        <Stack.Screen name='Foods' component={Foods} />
+        <Stack.Screen name='Recipe' component={Recipe} />  
+        </Stack.Navigator>    
+        </NavigationContainer>  
     )
 }
 
--- RecipeGeneratorApp/Foods.js
+++ RecipeGeneratorApp/Foods.js
@@ -1,18 +1,37 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
+export default function Foods() {
+  const [foods, setFoods] = useState ([])
 
-const [foods, setfoods] = useState 
+  const moreFood = () => {
+      fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+      .then((response) => response.json())
+        .then((json) => {  
+          console.log(json); 
+          const newFoods = json.meals;
+          setFoods((prev) => [...prev, ...newFoods]);   
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+  }
+  return (
+    <View>
+      <Text>Generate a recipe!</Text>
+      <Button title = "Give me dinner ideas" onPress = {moreFood}/>
+      <FlatList>
+        data={foods}
+        keyExtractor={(item) => item.idMeal}
+        renderItem={({item}) => (
+          <Text>
+            {item.strMeal}
+          </Text>
+        )}
+      </FlatList>
 
-const moreFood() => {
-    fetch('www.themealdb.com/api/json/v1/1/random.php')
-    .then((response) => response.json())
-      .then((json) => {  
-        console.log(json); 
-        const newFoods = json.foods;
-        setMovies((prev) => [...prev, ...newFoods]);   
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    </View>
+  )
+  
 }
